eb88be3f405bbb0c295d248d16d9ef26c5de0362
Sameer Suman <sameersuman2403@gmail.com>
2023-03-02 10:52:41 +0530

    Co-authored-by: akrockzz123 <akrockzz123@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,32 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div> hello world</div>;
+import { Container } from "react-bootstrap";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+LoginScreen
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path = "/admin/login" element = {<AdminLoginScreen/>} exact />
+            <Route path = "/user/login" element = {<UserLoginScreen/>} exact />
+            <Routes/>
+        <Container/>
+      </main>
+      </div>
+         
+  );
 }
 
 export default App;
+
+            
